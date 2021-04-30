fef34b7655744c58a3f5cb8f9668734a95ec3fec
fernando471 <82480329+fernando471@users.noreply.github.com>
2021-04-30 08:21:00 +0500

    Add files via upload

---
+++ Final.Core/Entities/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Final.Core.Entities
+{
+    public class Post
+    {
+        public int PostId { get; set;}
+
+        public int IdUser { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description  { get; set; }
+
+        public string Image { get; set; }
+
+    }
+}
---
+++ Final.Core/Interfaces/IPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using Final.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final.Core.Interfaces
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<Post>> GetPosts();
+    }
+}
